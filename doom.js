88a5e5163984cec4245b9ac14697001145aab4e9
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-04-21 08:53:26 -0500

    Se crean las const de elemento para titulo y texto

--- doom.js
+++ doom.js
@@ -7,4 +7,14 @@ function createPost (name, message, imageURL) {
     elemento.setAttribute("src", imageURL);
     div.append(elemento);
 
+    const titulo = document.createElement("h1")
+    titulo.textContent = name
+    div.append(titulo);
+
+    const texto = document.createElement("p")
+    texto.textContent = message
+    div.append(texto);
+
+    return div;
+
 }
