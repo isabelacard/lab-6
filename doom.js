02fcedd2ba074227ddbcb54d27e5be57860c81b2
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-04-21 08:52:40 -0500

    Se crea la const de elemento para imageURL

--- doom.js
+++ doom.js
@@ -3,4 +3,8 @@ function createPost (name, message, imageURL) {
     const div = document.createElement("div");
     div.classList.add("character");
 
+    const elemento = document.createElement("img");
+    elemento.setAttribute("src", imageURL);
+    div.append(elemento);
+
 }
